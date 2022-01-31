ad93d9ef27ee804ce325b9e521116c92e393cd48
SAMTECHNET <samlaja1292@gmail.com>
2022-01-31 21:54:18 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const {cut3, largeNumber} = require("../utilities/util1");
+
+
+const {sum, concat} = require('../utilities/util2');
+
+
+const findLargeNumber = [1, 2, 3, 4, 5]
+
+
+const addArrayNumber = [3, 4, 5, 6, 7]
+
+console.log(cut3([3, 4, 5, 6, 7]));
+console.log(largeNumber([3, 4, 5, 6, 7]))
+console.log(sum(addArrayNumber))
+console.log(concat(findLargeNumber, addArrayNumber))
